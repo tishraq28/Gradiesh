69c1ed581b8876ffe9f22d49090680809c3f7ac3
Ishraq Tanvir <101325013+tishraq28@users.noreply.github.com>
2022-05-05 17:51:30 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+var btn = document.getElementById("n1");
+var clipboard = new ClipboardJS(n1);
+
+var btn = document.getElementById("n2");
+var clipboard = new ClipboardJS(n2);
+
+var btn = document.getElementById("n3");
+var clipboard = new ClipboardJS(n3);
+
+var btn = document.getElementById("n4");
+var clipboard = new ClipboardJS(n4);
+
+var btn = document.getElementById("n5");
+var clipboard = new ClipboardJS(n5);
+
+var btn = document.getElementById("n6");
+var clipboard = new ClipboardJS(n6);
+
+var btn = document.getElementById("n7");
+var clipboard = new ClipboardJS(n7);
+
+var btn = document.getElementById("n8");
+var clipboard = new ClipboardJS(n8);
+
+var btn = document.getElementById("n9");
+var clipboard = new ClipboardJS(n9);
+
+var btn = document.getElementById("n10");
+var clipboard = new ClipboardJS(n10);
+
+var btn = document.getElementById("n11");
+var clipboard = new ClipboardJS(n11);
+
+var btn = document.getElementById("n12");
+var clipboard = new ClipboardJS(n12);
+
+var btn = document.getElementById("n13");
+var clipboard = new ClipboardJS(n13);
+
+var btn = document.getElementById("n14");
+var clipboard = new ClipboardJS(n14);
+
+var btn = document.getElementById("n15");
+var clipboard = new ClipboardJS(n15);
+
+var btn = document.getElementById("n16");
+var clipboard = new ClipboardJS(n16);
+
+var btn = document.getElementById("n17");
+var clipboard = new ClipboardJS(n17);
+
+var btn = document.getElementById("n18");
+var clipboard = new ClipboardJS(n18);
+
+var btn = document.getElementById("n19");
+var clipboard = new ClipboardJS(n19);
+
+var btn = document.getElementById("n20");
+var clipboard = new ClipboardJS(n20);
+
+var btn = document.getElementById("n21");
+var clipboard = new ClipboardJS(n21);
+
+var btn = document.getElementById("n22");
+var clipboard = new ClipboardJS(n22);
+
+var btn = document.getElementById("n23");
+var clipboard = new ClipboardJS(n23);
+
+var btn = document.getElementById("n24");
+var clipboard = new ClipboardJS(n24);
+
+function sw() {
+  swal({
+    title: "Success!",
+    text: "You have successfully copied code!",
+    icon: "success",
+    button: "OK!",
+  });
+}
